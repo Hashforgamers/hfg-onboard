e3eb9759846a11ef0666400db071241af459366b
bhanujoshi <jbhanu244@gmail.com>
2025-06-27 00:30:28 +0200

    part 3: Fixed multi Cafe

--- models/vendorPin.py
+++ models/vendorPin.py
@@ -6,7 +6,7 @@ class VendorPin(db.Model):
     __tablename__ = 'vendor_pins'
 
     id = Column(Integer, primary_key=True)
-    vendor_id = Column(Integer, ForeignKey('vendor.id'), nullable=False)
+    vendor_id = Column(Integer, ForeignKey('vendors.id'), nullable=False)
     pin_code = Column(String(6), unique=True, nullable=False)
 
     vendor = relationship('Vendor', back_populates='pin')
