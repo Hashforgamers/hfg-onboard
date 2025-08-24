761231a3cb1202ebd5ad003f05df6d23c94063ad
prshnt-hash <prashant.rajput@hashforgamers.com>
2025-08-24 11:46:33 +0530

    added otp service while accessing bank tranfer and added deletion route for cafe images

--- app/config.py
+++ app/config.py
@@ -26,3 +26,7 @@ class Config:
     CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
     CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
     CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
+    
+     # Redis Configuration
+    REDIS_URL = os.getenv('REDIS_URL')
+    REDIS_TLS_ENABLED = os.getenv('REDIS_TLS_ENABLED', 'false').lower() == 'true'
