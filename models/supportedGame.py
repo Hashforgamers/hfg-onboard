e4f5e39c2deb10af7eaf1b0e6ec3bafb5dd3f335
Prashant <prashant.rajput@hashforgamers.com>
2026-02-17 20:45:36 +0000

    fixed supportedGame

---
+++ models/supportedGame.py
@@ -0,0 +1,26 @@
+# models/supportedGame.py
+from sqlalchemy import Column, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from db.extensions import db
+from sqlalchemy import Enum
+import enum
+
+
+class PlatformEnum(enum.Enum):
+    pc = 'pc'
+    ps = 'ps'
+    vr = 'vr'
+    xbox = 'xbox'
+
+
+class SupportedGame(db.Model):
+    __tablename__ = 'supported_games'
+    id = Column(Integer, primary_key=True)
+    vendor_id = Column(Integer, ForeignKey('vendors.id'), nullable=False)
+    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+    platform = Column(Enum(PlatformEnum), nullable=False)
+    is_famous = Column(Boolean, default=False)
+
+    # ✅ REMOVE back_populates from both relationships
+    vendor = relationship('Vendor')  # No back_populates
+    game = relationship('Game')      # No back_populates
